b40d747a8e34cbc1b018ea505424c9a769c034ca
Erhan Abay <erhanabay@gmail.com>
2013-10-14 17:24:45 +0300

    projenin dağıtılabilen versiyonu için değişiklikler yapıldı.
    grunt build görevi sırasıyla tüm işlemleri gösteriyor ama kısaca:
    
    * tüm dosyalar tmp altına kopyalanıyor
    * styles.scss dosyası ile tek bir stylesheet dosyası oluşturuluyor.
    * requirejs görevi ile proje tek bir script dosyasına indirgeniyor.
     * htmlrefs ile styleheet ve script dosyalarına referanslar yenileri ile güncelleniyor.
     * son olarak da sadece gerekli dosyalar dist klasörüne kopyalanıyor.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,13 @@
 module.exports = function(grunt) {
     grunt.initConfig({
+        paths: {
+            src: 'src',
+            tmp: 'tmp',
+            dist: 'dist'
+        },
         concurrent: {
             target: {
-                tasks: ['watch', 'connect'],
+                tasks: ['watch', 'connect:dev'],
                 options: {
                     logConcurrentOutput: true
                 }
@@ -10,27 +15,77 @@ module.exports = function(grunt) {
         },
         watch: {
             css: {
-                files: 'src/assets/sass/*.scss',
-                tasks: ['compass']
+                files: '<%= paths.src %>/assets/sass/*.scss',
+                tasks: ['compass:dev']
             }
         },
         compass: {
             dev: {
                 options: {
-                    basePath: 'src/assets',
+                    basePath: '<%= paths.src %>/assets',
                     httpPath: '../',
                     cssDir: 'css',
                     environment: 'development'
                 }
+            },
+            dist: {
+                options: {
+                    basePath: '<%= paths.tmp %>/assets',
+                    cssDir: '../',
+                    specify: '<%= paths.tmp %>/assets/sass/styles.scss',
+                    outputStyle: 'compressed'
+                }
             }
         },
         connect: {
-            server: {
+            dev: {
                 options: {
-                    base: 'src',
-                    hostname: '*',
-                    keepalive: true
+                    base: '<%= paths.src %>'
                 }
+            },
+            dist: {
+                options: {
+                    base: '<%= paths.dist %>'
+                }
+            },
+            options: {
+                hostname: '*',
+                keepalive: true
+            }
+        },
+        clean: {
+            tmp: '<%= paths.tmp %>',
+            dist: '<%= paths.dist %>'
+        },
+        copy: {
+            tmp: {
+                cwd: '<%= paths.src %>',
+                src: '**',
+                dest: '<%= paths.tmp %>',
+                expand: true
+            },
+            dist: {
+                cwd: '<%= paths.tmp %>',
+                src: ['index.html', 'scripts.js', 'styles.css'],
+                dest: '<%= paths.dist %>',
+                expand: true
+            }
+        },
+        requirejs: {
+            dist: {
+                options: {
+                    baseUrl: '<%= paths.tmp %>/assets',
+                    mainConfigFile: '<%= paths.tmp %>/assets/js/main.js',
+                    name: 'vendor/almond/almond',
+                    include: 'js/main',
+                    out: '<%= paths.tmp %>/scripts.js',
+                    optimize: 'uglify2'
+                }
+            }
+        },
+        htmlrefs: {
+            dist: {
+                src: '<%= paths.tmp %>/index.html'
             }
         }
     });
@@ -39,6 +94,11 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-htmlrefs');
 
     grunt.registerTask('default', ['concurrent']);
+    grunt.registerTask('build', ['clean', 'copy:tmp', 'compass:dist', 'requirejs', 'htmlrefs', 'copy:dist', 'clean:tmp']);
 };
